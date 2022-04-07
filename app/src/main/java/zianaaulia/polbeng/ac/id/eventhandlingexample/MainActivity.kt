18830f718cd721860c220f8426595e1f16e13374
Ziana Aulia <zianaaulia2602@gmail.com>
2022-04-07 11:34:02 +0700

    Memberikan fungsi logika di file MainActivity.kt

--- app/src/main/java/zianaaulia/polbeng/ac/id/eventhandlingexample/MainActivity.kt
+++ app/src/main/java/zianaaulia/polbeng/ac/id/eventhandlingexample/MainActivity.kt
@@ -2,10 +2,20 @@ package zianaaulia.polbeng.ac.id.eventhandlingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDisplayMessage.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View) {
+                Toast.makeText(applicationContext,"Ziana Aulia",
+                Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
