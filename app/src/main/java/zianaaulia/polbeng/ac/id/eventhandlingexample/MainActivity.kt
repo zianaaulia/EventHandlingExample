cd3290b55525444c97a30b6166d71072721fe022
Ziana Aulia <zianaaulia2602@gmail.com>
2022-04-07 11:45:43 +0700

    Memberikan fungsi logika di file MainActivity.kt

--- app/src/main/java/zianaaulia/polbeng/ac/id/eventhandlingexample/MainActivity.kt
+++ app/src/main/java/zianaaulia/polbeng/ac/id/eventhandlingexample/MainActivity.kt
@@ -2,8 +2,8 @@ package zianaaulia.polbeng.ac.id.eventhandlingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,16 +14,15 @@ class MainActivity : AppCompatActivity() {
         //Menampilkan pesan setelah di klik
         btnDisplayMessage.setOnClickListener {
             Toast.makeText(
-                applicationContext, "Ziana Aulia",
+                applicationContext, "Hello World",
                 Toast.LENGTH_LONG
             ).show()
         }
         //Fungsi untuk menangani event onLongClickListener
-        btnDisplayMessage.setOnLongClickListener(object: View.OnLongClickListener
-        {
-            override fun onLongClick(v: View?): Boolean {
-                return true
-            }
-        })
+        btnDisplayMessage.setOnLongClickListener {
+            Snackbar.make(root_layout, "Long click",
+                Snackbar.LENGTH_LONG).show()
+            true
+        }
     }
 }
